8c0055f69207a4d28976c2540ae2efca33c62915
James Dickson <james_dickson1@baylor.edu>
2022-04-13 15:38:10 -0500

    commit

--- webscraping-Bible.py
+++ webscraping-Bible.py
@@ -3,10 +3,32 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, Request
 
+chapters = ['01','02','03','04','05','06','07','08','09','10']
 
+rand_chap = random.choice(chapters)
 
-webpage = 'https://ebible.org/asv/JHN'
+url = 'https://ebible.org/asv/JHN' + rand_chap + '.htm'
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
-req = Request(webpage, headers=headers)
+req = Request(url, headers=headers)
 
+
+webpage = urlopen(req).read()
+
+soup = BeautifulSoup(webpage, 'html.parser')
+
+allverses = soup.findAll('div',class_='p')
+
+allverses = []
+
+myverses = []
+
+for verse in allverses:
+    #print(verse.text.split("  "))
+    for v in verse.text.split("  "):
+        myverses.append(v)
+    input()
+
+mychoice = random.choice(myverses)
+
+print('Chapter:',rand_chap,'verse:',mychoice)
--- webscraping-movies.py
+++ webscraping-movies.py
@@ -17,9 +17,45 @@ soup = BeautifulSoup(page, 'html.parser')
 
 title = soup.title
 
-print(title.text)
 ##
 ##
 ##
 ##
 
+wb = xl.Workbook()
+ms = wb.active
+ms.title = 'First Sheet'
+
+mv_table = soup.find('table')
+
+mv_rows = mv_table.findAll('tr')
+
+#print(mv_rows[1])
+
+ms['A1'] = "No."
+ms['B1'] = "Title"
+ms['C1'] = 'Gross'
+ms['D1'] = 'Total Gross'
+ms['E1'] = "Percent of Gross"
+
+ 
+
+
+wb.save('Box Office Report.xlsx')
+
+for x in range(1,6):
+    td = mv_rows[x].findAll('td')
+    rank = td[0].text
+    title = td[1].text
+    gross = int(td[5].text.replace(",","").replace("$",""))
+    t_gross = int(td[7].text.replace(",","").replace("$",""))
+
+    percent = round((gross/t_gross)*100,2)
+    
+    ms['A' + str(x+1)] = rank
+    ms['B' + str(x+1)] = title
+    ms['C' + str(x+1)] = gross
+    ms['D' + str(x+1)] = t_gross
+    ms['E' + str(x+1)] = str(percent) + "%"
+    
+    wb.save('Box Office Report.xlsx')
